0c2302119e9dbb34ea40bbe7251bde146161a827
Igor Smirnov <uchetzapis@yahoo.com>
2023-04-16 21:53:04 +0300

    Parse name and link for profile for each director / actor / etc

--- apps/parser/src/parser.service.ts
+++ apps/parser/src/parser.service.ts
@@ -1,85 +1,42 @@
 import { Injectable } from '@nestjs/common';
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 
 
 const titleXpath = '//*[@id="__next"]/div[2]/div[2]/div[2]/div[2]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/h1/span';
 const genreXpath = '//a[contains(@href, "lists/movies/genre--")]';
 const timeXpath = '//*[@id="__next"]/div[2]/div[2]/div[2]/div[2]/div/div[3]/div/div/div[2]/div[1]/div/div[23]/div[2]/div';
 
-const directorTag = `a[@name="director"]`;
-const actorTag = 'a[@name="actor"]';
-const producerTag = 'a[@name="producer"]';
-const voiceDirectorTag = 'a[@name="voice_director"]';
-const translatorTag = 'a[@name="translator"]';
-const voiceTag = 'a[@name="voice"]';
-const writerTag = 'a[@anm="writer"]';
-const operatorTag = 'a[@name="operator"]';
-const composerTag = 'a[@name="composer"]';
-const designTag = 'a[@name="design"]';
-const editorTag = 'a[@name="deitor"]';
 
-function foo(n) {
-  console.log(n);
-  
+type Role = 'director' | 'actor' | 'producer' | 'voice_director' | 'translator' | 'voice' | 'writer'
+  | 'operator' | 'composer' | 'design' | 'editor';
+
+const directorsXpath = xpathBeetweenRoles('director', 'actor');
+const actorsXpath = xpathBeetweenRoles('actor', 'producer');
+const producersXpath = xpathBeetweenRoles('producer', 'voice_director');
+const voiceDirectorsXpath = xpathBeetweenRoles('voice_director', 'translator');
+const translatorsXpath = xpathBeetweenRoles('translator', 'voice');
+const voicesXpath = xpathBeetweenRoles('voice', 'writer');
+const writersXpath = xpathBeetweenRoles('writer', 'operator');
+const operatorsXpath = xpathBeetweenRoles('operator', 'composer');
+const composersXpath = xpathBeetweenRoles('composer', 'design');
+const designersXpath = xpathBeetweenRoles('design', 'editor');
+const editorsXpath = '//a[@name="editor"]/following-sibling::div//div[@class="actorInfo"]';
+
+
+function xpathBetween(tagBefore: string, tagAfter: string): string {
+  return `[preceding-sibling::${tagBefore} and not(preceding-sibling::${tagAfter})]`;
 }
+
+function xpathBeetweenRoles(roleFrom: Role, roleTo: Role) {
+  const tagBefore = `a[@name="${roleFrom}"]`;
+  const tagAfter = `a[@name="${roleTo}"]`;
+
+  return `//div${xpathBetween(tagBefore, tagAfter)}//div[@class="actorInfo"]`;
+}
+
 
 @Injectable()
 export class ParserService {
-
-  private xpathBetween(tagBefore, tagAfter): string {
-    return `[preceding-sibling::${tagBefore} and not(preceding-sibling::${tagAfter})]`;
-  }
-
-  private actorInfo(tagBefore, tagAfter) {
-    return `//div${this.xpathBetween(tagBefore, tagAfter)}//div[@class="actorInfo"]`;
-  }
-
-  get directorXpath() {
-    return this.actorInfo(directorTag, actorTag);
-  }
-
-  get actorXpath() {
-    return this.actorInfo(actorTag, producerTag);
-  }
-
-  get proudcerXpath() {
-    return this.actorInfo(producerTag, voiceDirectorTag); 
-  }
-
-  get voiceDirectorXpath() {
-    return this.actorInfo(voiceDirectorTag, translatorTag);
-  }
-
-  get translatorXpath() {
-    return this.actorInfo(translatorTag, voiceTag);
-  }
-
-  get voiceXpath() {
-    return this.actorInfo(voiceTag, writerTag);
-  }
-
-  get writerXpath() {
-    return this.actorInfo(writerTag, operatorTag);
-  }
-
-  get operatorXpath() {
-    return this.actorInfo(operatorTag, composerTag);
-  }
-
-  get composerXpath() {
-    return this.actorInfo(composerTag, designTag);
-  }
-
-  get designXpath() {
-    return this.actorInfo(designTag, editorTag);
-  }
-
-  get editorXpath() {
-    return `${editorTag}//following-sibling::div//div[@clas="actorInfo]`
-  }
-  
-
-
 
   async parse(film: number = 535341) {
     const mainUrl = `https://www.kinopoisk.ru/film/${film}/`;
@@ -97,11 +54,12 @@ export class ParserService {
 
     try {
 
-      const fullTitle = await (
+      const { title, year} = await (
         await page.waitForXPath(titleXpath)
-      ).evaluate((node) => node.textContent);
+      ).evaluate((node) => node.textContent.match(/(?<title>.*) \((?<year>\d{4})\)/).groups);
       
-      console.log(fullTitle);
+      console.log(title);
+      console.log(year);
 
       const genres = await Promise.all(
         await (
@@ -113,36 +71,23 @@ export class ParserService {
      
       console.log(genres);
 
-      const time = await (
+      const duration = await (
         await page.waitForXPath(timeXpath)
       ).evaluate((node) => node.textContent);
 
-      console.log(time);
+      console.log(duration);
 
-      await page.goto(actorsUrl);
+      await page.goto(actorsUrl, {
+        waitUntil: 'domcontentloaded'
+      });
 
-      console.log(`$x('${this.directorXpath}')`);
-      console.log(`$x(${this.actorXpath}')`)
-
-      const directors = await page.$x(this.directorXpath);
-
-      console.log(directors);
-
-      const actor = await page.$x(this.actorXpath);
-      console.log(actor);
-
-      const producers = await page.$x(this.proudcerXpath);
-      const voiceDirectors = await page.$x(this.voiceDirectorXpath);
-      const translatrors = await page.$x(this.translatorXpath);
-      const voices = await page.$x(this.voiceXpath);
-      const writers = await page.$x(this.writerXpath);
-      const operators = await page.$x(this.operatorXpath);
-      const composers = await page.$x(this.composerXpath);
-      const designers = await page.$x(this.designXpath);
-      const editors = await page.$x(this.editorXpath);
+      const persons = await this.getPersons(page);
 
       // page.goto(voicesUrl);
-
+      
+      const res = { title, year, genres, duration, persons };
+      console.log(res);
+      return res;
     } catch (e) {
       console.log(e)
     }
@@ -151,4 +96,53 @@ export class ParserService {
       await browser.close();
     }
   }
+
+  async getPersons(page: Page) {
+    const directors = await this.getPersonsWithRole(page, directorsXpath);
+    console.log('direcors:\n\n')
+    console.log(directors);
+    const actors = await this.getPersonsWithRole(page, actorsXpath);
+    console.log('actors:\n\n')
+    console.log(actors);
+    const producers = await this.getPersonsWithRole(page, producersXpath);
+    const voiceDirectors = await this.getPersonsWithRole(page, voiceDirectorsXpath);
+    const translators = await this.getPersonsWithRole(page, translatorsXpath);
+    const voices = await this.getPersonsWithRole(page, voicesXpath);
+    const writers = await this.getPersonsWithRole(page, writersXpath);
+    const operators = await this.getPersonsWithRole(page, operatorsXpath);
+    const composers = await this.getPersonsWithRole(page, composersXpath);
+    const designers = await this.getPersonsWithRole(page, designersXpath);
+    const editors = await this.getPersonsWithRole(page, editorsXpath);
+
+    return { directors, actors, producers, voiceDirectors, translators, voices, writers, operators, composers, designers, editors };
+  }
+
+  private async getPersonsWithRole(page: Page, xpath: string) {
+
+    const persons = await Promise.all(
+      (await page.$x(xpath))
+        .map(async (d) => {
+          const nameHandler = await d.$('div.info>div.name');
+
+          const { url, name } = await nameHandler.$('a').then((handle) => handle.evaluate((node) => ({ url: node.getAttribute('href'), name: node.text })));
+          // console.log({ url, name });
+          const name_en = await nameHandler.$('span').then((handle) => handle.evaluate((node) => node.textContent));
+          // console.log(name_en);
+          // const photo = await d.$('div.photo>a>img').then((handle) => handle.evaluate((node) => node.getAttribute('src')));
+
+          const res = {
+            url,
+            name,
+            name_en,
+            // photo,
+          };
+           console.log(res);
+          return res;
+        })
+    );
+
+    return persons;
+  }
+
+
 }
