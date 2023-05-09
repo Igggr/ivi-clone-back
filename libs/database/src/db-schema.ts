9b5360124a2bcff015dd5ac60783b97305c22d10
Igor Smirnov <uchetzapis@yahoo.com>
2023-05-09 22:07:32 +0300

    Remove duplicated env keys

--- libs/database/src/db-schema.ts
+++ libs/database/src/db-schema.ts
@@ -3,7 +3,7 @@ import * as Joi from 'joi';
 export const db_schema = Joi.object({
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().required(),
-  DB_USER: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  DB_NAME: Joi.string().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
 });
