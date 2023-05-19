2f57fbcbefc10567ce2d311a4f6c5d909dd81bb0
Igor Smirnov <uchetzapis@yahoo.com>
2023-05-19 16:19:57 +0300

    Microservice should be able to connect to rabbit no matter how it started - from compose or command-line

--- libs/rabbit/src/options.ts
+++ libs/rabbit/src/options.ts
@@ -3,7 +3,10 @@ import { Transport } from '@nestjs/microservices';
 export const RABBIT_OPTIONS = (name: string) => ({
   transport: Transport.RMQ as const,
   options: {
-    urls: ['amqp://localhost:5672'],
+    urls: [
+      'amqp://rabbit:5672',    // микросервис заппущен из контейнера
+      'amqp://localhost:5672', // микросервис заппущен из командной строки
+    ],
     queue: `${name}_queue`,
     queueOptions: {
       durable: true,
