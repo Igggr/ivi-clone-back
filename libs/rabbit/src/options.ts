7d35343a0401e576783a763d4c07f96d6293af0e
Igor Smirnov <uchetzapis@yahoo.com>
2023-05-19 16:31:28 +0300

    Should run test for pull-request in master

--- libs/rabbit/src/options.ts
+++ libs/rabbit/src/options.ts
@@ -4,7 +4,7 @@ export const RABBIT_OPTIONS = (name: string) => ({
   transport: Transport.RMQ as const,
   options: {
     urls: [
-      'amqp://rabbit:5672',    // микросервис заппущен из контейнера
+      'amqp://rabbit:5672', // микросервис заппущен из контейнера
       'amqp://localhost:5672', // микросервис заппущен из командной строки
     ],
     queue: `${name}_queue`,
