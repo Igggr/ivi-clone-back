076c6d8a534177312b06d96a5f5071070728f7cc
knesp <knesp@yandex.ru>
2023-05-21 01:18:21 +0400

    fixed createUserProfileDto

--- libs/shared/src/dto/create-user-profile.dto.ts
+++ libs/shared/src/dto/create-user-profile.dto.ts
@@ -3,6 +3,6 @@ import { Profile } from '../entities/profile.entity';
 import { User } from '../entities/user.entity';
 
 export class CreateUserProfileDto extends IntersectionType(
-  OmitType(User, ['id']),
+  OmitType(User, ['id', 'roles']),
   OmitType(Profile, ['id', 'userId', 'createdAt']),
 ) {}
