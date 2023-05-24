64b6ac8f86d35d71469d424f7ad28969c636916a
Iggr <33826919+Igggr@users.noreply.github.com>
2023-05-24 18:55:13 +0300

    Add e2e tests for auth (#16)
    Co-authored-by: knesp <knesp@yandex.ru>

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -10,24 +10,41 @@ export class DatabaseModule {
         module: DatabaseModule,
         imports: [
           TypeOrmModule.forRootAsync({
-            useFactory: (configService: ConfigService) => ({
-              type: 'postgres',
-              host:
-                configService.get<string>('FOR') === 'docker'
-                  ? configService.get<string>('DOCKER_DB')
-                  : configService.get<string>('DB_HOST'),
-              port:
-                configService.get('FOR') === 'docker'
-                  ? 5432
-                  : +configService.get('DB_PORT'),
-              username: configService.get<string>('POSTGRES_USER'),
-              password: configService.get<string>('POSTGRES_PASSWORD'),
-              database: configService.get<string>('POSTGRES_DB'),
-              entities,
-              synchronize: configService.get('NODE_ENV') !== 'prod' || true,
-              autoLoadEntities:
-                configService.get('NODE_ENV') !== 'prod' || true,
-            }),
+            useFactory: (configService: ConfigService) => {
+              if (configService.get('IS_TEST')) {
+                console.log('Test DB');
+                return {
+                  type: 'postgres',
+                  host: 'postgres_test',
+                  port: 5432,
+                  username: 'test_user',
+                  password: 'test_password',
+                  database: 'test_db',
+                  entities: entities,
+                  synchronize: true,
+                  autoLoadEntities: true,
+                };
+              }
+              console.log('Normal DB');
+              return {
+                type: 'postgres',
+                host:
+                  configService.get<string>('FOR') === 'docker'
+                    ? configService.get<string>('DOCKER_DB')
+                    : configService.get<string>('DB_HOST'),
+                port:
+                  configService.get('FOR') === 'docker'
+                    ? 5432
+                    : +configService.get('DB_PORT'),
+                username: configService.get<string>('POSTGRES_USER'),
+                password: configService.get<string>('POSTGRES_PASSWORD'),
+                database: configService.get<string>('POSTGRES_DB'),
+                entities,
+                synchronize: configService.get('NODE_ENV') !== 'prod' || true,
+                autoLoadEntities:
+                  configService.get('NODE_ENV') !== 'prod' || true,
+              };
+            },
             inject: [ConfigService],
           }),
         ],
