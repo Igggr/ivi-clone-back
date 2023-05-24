1b67ea9043b31faed58fc9d39a95d0fa301779cb
Igor Smirnov <uchetzapis@yahoo.com>
2023-05-24 11:48:09 +0300

    Fix tests

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,51 +1,62 @@
-import { INestApplication } from "@nestjs/common";
+import { HttpStatus, INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import * as request from 'supertest';
 import { ApiModule } from "../src/api.module";
-import { CreateUserProfileDto } from "@app/shared";
-import { ConfigService } from "@nestjs/config";
+import { HttpExceptionFilter } from "@app/shared";
 
 describe('Registration', () => {
     let app: INestApplication;
 
+    const firstUserDTO = {
+        name: 'first',
+        surname: 'user',
+        nickname: 'pervonach',
+        email: "firstuser@mail.com",
+        password: '1111',
+    };
+
+    const secondUserDTO = {
+        name: 'second',
+        surname: 'user',
+        nickname: 'vtoroy',
+        email: "seconduser@mail.com",
+        password: '2222',
+    };
+
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
-            imports: [ApiModule,
-                // AuthModule, ProfilesModule
-            ],
-        }).overrideProvider(ConfigService).useValue({
-            get(key) {
-                if (key === 'IS_TEST') {
-                    return true;
-            } 
-        }})
-            .compile();
+            imports: [ApiModule],
+        }).compile();
 
         app = moduleRef.createNestApplication();
+        app.useGlobalFilters(new HttpExceptionFilter());
         await app.init();
     });
 
-    it(`/POST registration`, () => {
-        const dto: CreateUserProfileDto = {
-            email: "firstuser@mail.com",
-            password: '1111',
-            name: 'Jack',
-            surname: 'Kent',
-            nickname: 'jackiee',
-            country: 'Usa',
-            city: 'Chickago',
-            url: null,
-            photo: null
-        };
+    it(`/POST registration: user can register`, () => {
         return request(app.getHttpServer())
             .post('/registration')
-            .send(dto)
-            .expect(400)
-            // .expect({
-            //     status: 'ok',
-            // })
-            .then((r) => console.log(r));
-    });
+            .send(firstUserDTO)
+            .expect(HttpStatus.CREATED)
+            .then((r) => expect(r.body.token).toBeDefined());
+    }, 100000);
+
+    it(`/POST registration: only one user can register with email`, () => {
+        return request(app.getHttpServer())
+            .post('/registration')
+            .send(firstUserDTO)
+            .expect(HttpStatus.BAD_REQUEST)
+            .then((r) => expect(r.body.error).toBe("Пользователь с таким email уже существует"));
+    }, 100000);
+
+    it(`/POST registration: other users can register as well`, () => {
+        return request(app.getHttpServer())
+            .post('/registration')
+            .send(secondUserDTO)
+            .expect(HttpStatus.CREATED)
+            .then((r) => expect(r.body.token).toBeDefined());
+    }, 100000);
+
 
     afterAll(async () => {
         await app.close();
