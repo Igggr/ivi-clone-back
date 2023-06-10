15e0a52ff19975f5fdafeb974d5fd356663acc8a
Егор Кнестяпин <knesp@yandex.ru>
2023-06-10 20:26:00 +0400

    little fixes

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -173,13 +173,4 @@ export class AuthController {
       ),
     );
   }
-
-  // @Get('status')
-  // googleUser(@Req() request: Request) {
-  //   if (request.user) {
-  //     return { msg: 'Authenticated' };
-  //   } else {
-  //     return { msg: 'Not Authenticated' };
-  //   }
-  // }
 }
