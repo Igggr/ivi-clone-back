1ff37e24bdc1839d4a617775370de805ca2ec631
Igor Smirnov <uchetzapis@yahoo.com>
2023-06-11 18:58:35 +0300

    Do not need swagger.json anymore

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from '@app/shared';
-import * as fs from 'fs';
 import { BearerAuth } from './guards/bearer';
 import * as session from 'express-session';
 import * as passport from 'passport';
@@ -21,7 +20,6 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  fs.writeFileSync('swagger.json', JSON.stringify(document));
   SwaggerModule.setup('docs', app, document);
 
   app.use(
